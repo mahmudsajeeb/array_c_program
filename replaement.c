dc031340b8df16ff347c082c5d581c8cfd5cb0fd
mahmudsajeeb <saifurrahmansajib37@gmail.com>
2025-07-04 09:40:34 +0600

    Positions in arrayPositions in array

--- replaement.c
+++ replaement.c
@@ -2,22 +2,41 @@
 
 int main() {
     int n;
-     scanf("%d",&n);
-     int arr[n]; 
-     for(int i=0; i<n; i++){
-        scanf("%d", &arr[i]);
-     }
-     for(int i=0; i<n; i++){
-        if(arr[i]> 0){
+    //  scanf("%d",&n);
+    //  int arr[n]; 
+    //  for(int i=0; i<n; i++){
+    //     scanf("%d", &arr[i]);
+    //  }
+    //  for(int i=0; i<n; i++){
+    //     if(arr[i]> 0){
+    //         arr[i]=1;
+    //     }else if(arr[i]< 0){
+    //         arr[i] =2;
+    //     }
+
+    //  }
+
+    //     for(int i=0; i<n; i++){
+    //         printf("%d ", arr[i]);
+    //     }
+
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+
+    for(int i =0;i<n; i++){
+        if(arr[i]>0){
             arr[i]=1;
-        }else if(arr[i]< 0){
-            arr[i] =2;
+        }else if(arr[i]<0){
+            arr[i]=2;
         }
+        
+    }
 
-     }
-
-        for(int i=0; i<n; i++){
-            printf("%d ", arr[i]);
-        }
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
